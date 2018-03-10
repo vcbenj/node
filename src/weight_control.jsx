b03e3f28b87ae6b86f14658865d7d19bf6978509
Matthew Burr <matt.d.burr@gmail.com>
2018-03-09 21:47:56 -0600

    Added header(s)

--- src/weight_control.jsx
+++ src/weight_control.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default function WeightControl(props) {
   return (
     <div className="form-group">
-      <label htmlFor="weight">Item Weight</label>
+      <label htmlFor="weight">Item Weight (oz.)</label>
       <input
         type="number"
         min="0.01"
