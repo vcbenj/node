f4e653807ee15727f7936e92da4fd9d0a89641a9
Matthew Burr <matt.d.burr@gmail.com>
2018-03-10 10:39:25 -0600

    Up-sized the postal rate, so it stands out

--- src/result.jsx
+++ src/result.jsx
@@ -8,8 +8,10 @@ export default function Result(props) {
       <div className="card-body">
         <h5 className="card-title">Postal Rate</h5>
         <p>
-          Your postal rate for {type} with weight {weight} is {rate}
+          Your postal rate for <strong>{type}</strong> with weight{' '}
+          <strong>{weight}</strong> oz. is:
         </p>
+        <h1 className="display-4">{rate}</h1>
         <Link to="/" className="btn btn-primary">
           Calculate Another
         </Link>
