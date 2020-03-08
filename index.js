e7651d8ec84cba4e52c11e6b29e38dc8dad1a3d9
vcbenj <whi15026@byui.edu>
2020-03-07 17:01:48 -0700

    Postal Calculator

--- index.js
+++ index.js
@@ -9,55 +9,13 @@ app.use(express.static(__dirname + '/public'));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
-// set up a rule that says requests to "/math" should be handled by the
-// handleMath function below
-app.get('/math', CalcFunction);
+app.get('/getRate', CalcFunction);
 
-// start the server listening
 app.listen(port, function() {
   console.log('Node app is running on port', port);
 });
 
 
-/**********************************************************************
- * Ideally the functions below here would go into a different file
- * but for ease of reading this example and seeing all of the pieces
- * they are listed here.
- **********************************************************************/
 
-function handleMath(request, response) {
-	const operation = request.query.operation;
-	const operand1 = Number(request.query.operand1);
-	const operand2 = Number(request.query.operand2);
 
-	// TODO: Here we should check to make sure we have all the correct parameters
-
-	computeOperation(response, operation, operand1, operand2);
-}
-
-function computeOperation(response, op, left, right) {
-	op = op.toLowerCase();
-
-	let result = 0;
-
-	if (op == "add") {
-		result = left + right;
-	} else if (op == "subtract") {
-		result = left - right;		
-	} else if (op == "multiply") {
-		result = left * right;
-	} else if (op == "divide") {
-		result = left / right;
-	} else {
-		// It would be best here to redirect to an "unknown operation"
-		// error page or something similar.
-	}
-
-	// Set up a JSON object of the values we want to pass along to the EJS result page
-	const params = {operation: op, left: left, right: right, result: result};
-
-	// Render the response, using the EJS page "result.ejs" in the pages directory
-	// Makes sure to pass it the parameters we need.
-	response.render('pages/result', params);
-
-}
+	response.render('results', params);
