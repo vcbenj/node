d4a13b9c4b65959393e23d8af866c6f40e1623d2
vcbenj <whi15026@byui.edu>
2020-03-07 18:33:05 -0700

    Postal Calculator

--- index.js
+++ index.js
@@ -3,19 +3,25 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 // tell it to use the public directory as one where static files live
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '\'public'));
 
-// views is directory for all template files
-app.set('views', __dirname + '/views');
+app.set('views', __dirname + '\views');
 app.set('view engine', 'ejs');
 
-app.get('/getRate', CalcFunction);
+// views is directory for all template files
+app.get("/", function(req, res) {
+	console.log("Received a request for /");
+
+	res.write("This is the root.");
+	res.end();
+});
+
+
+app.get('/getRate', function(req, res) {
+  res.render('results');
+});
 
 app.listen(port, function() {
   console.log('Node app is running on port', port);
 });
 
-
-
-
-	response.render('results', params);
